33a68a34c772192023704a493ad5dd23584c3a81
MBS <it@stats.gov.mv>
2025-02-28 18:12:01 +0500

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     //Route::get('/', function () { return view('welcome');})->name('home');
+    Route::get('/activity', RunnerActivity::class)->name('activity');
     Route::get('/', Leaderboard::class)->name('leaderboard');
     Route::get('/login', Login::class)->name('login');
     Route::get('/register', Register::class)->name('register');
@@ -33,6 +34,6 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
     Route::get('/addrun', AddRun::class)->name('addrun');
-    Route::get('/', Leaderboard::class)->name('leaderboard');
-    Route::get('/activity', RunnerActivity::class)->name('activity');
+    //Route::get('/', Leaderboard::class)->name('leaderboard');
+    //Route::get('/activity', RunnerActivity::class)->name('activity');
 });
